54e54f3f1132b4b8c26b7d2e4770098e03e3cab7
surfiniaburger <jdmasciano2@gmail.com>
2024-03-02 19:34:43 +0100

    more tasks for train_action.py

--- cairo_model/initializers/node_const_fold_opt_139/src/chunk0.cairo
+++
@@ -1,5 +0,0 @@
-
-
-fn compute(ref a: Array<u32>) {
-a.append(0);
-}
--- pool/train_action.py
+++ pool/train_action.py
@@ -8,7 +8,7 @@ os.environ['SSL_CERT_FILE'] = certifi.where()
 import pandas as pd
 import polars as pl
 import tf2onnx
-
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
 from sklearn.preprocessing import StandardScaler
@@ -18,86 +18,99 @@ from tensorflow.keras.layers import Input, Conv1D, Activation, Add
 import onnx
 from typing import List
 
+@task(name="Define Model Hyperparameters")
+def define_model_hyperparameters():
+    selected_columns = ['trading_volume_usd', 'blockchain_arbitrum', 'blockchain_avalanche_c', 'blockchain_base', 'blockchain_ethereum', 'blockchain_gnosis', 'blockchain_optimism', 'blockchain_polygon', 'token_pair_wstETH-wUSDM', 'token_pair_xSNXa-YFI', 'token_pair_yCURVE-YFI', 'day_of_week', 'month', 'year']
+    window_size = 30
+    input_shape = (window_size, len(selected_columns))
+    num_filters = 32
+    num_blocks = 5
+    kernel_size = 8
+    num_classes = 1  # Assuming regression task, change to number of classes for classification task
+    batch_size = 32
+
+    return input_shape, num_filters, num_blocks, kernel_size, num_classes, batch_size
+
+@task(name="Load Data")
+def load_data():
+    loader = DatasetsLoader()
+    # Load data from Polar into a DataFrame
+    df_polar = loader.load('balancer-daily-trade-volume')
+    return df_polar
+
+@task(name="Preprocess Data")
+def preprocess_data(df_polar):
+    # Extracting data from the Polar DataFrame
+    data = {
+        'day': df_polar['day'],
+        'pool_id': df_polar['pool_id'],
+        'blockchain': df_polar['blockchain'],
+        'token_pair': df_polar['token_pair'],
+        'trading_volume_usd': df_polar['trading_volume_usd']
+    }
+
+    # Creating a new Pandas DataFrame
+    df_pandas = pd.DataFrame(data)
+
+    # Perform one-hot encoding for categorical variables
+    df_encoded = pd.get_dummies(df_pandas, columns=['blockchain', 'token_pair'])
+
+    # Initialize StandardScaler
+    standard_scaler = StandardScaler()
+
+    # Perform Standardization on numerical features
+    df_encoded[['trading_volume_usd']] = standard_scaler.fit_transform(df_encoded[['trading_volume_usd']])
+
+    # Convert 'day' column to datetime format
+    df_encoded['day'] = pd.to_datetime(df_encoded['day'])
+
+    # Extract relevant features: day of the week, month, and year
+    df_encoded['day_of_week'] = df_encoded['day'].dt.dayofweek
+    df_encoded['month'] = df_encoded['day'].dt.month
+    df_encoded['year'] = df_encoded['day'].dt.year
+
+    return df_encoded
+
+@task(name="Create Sequences")
+def create_sequences(df_encoded, window_size):
+    # Calculate the total number of data points
+    total_data_points = df_encoded.shape[0]
+
+    # Calculate the total number of sequences
+    total_sequences = total_data_points - window_size + 1
+
+    # Select only necessary columns from the DataFrame
+    selected_columns = ['trading_volume_usd', 'blockchain_arbitrum', 'blockchain_avalanche_c', 'blockchain_base', 'blockchain_ethereum', 'blockchain_gnosis', 'blockchain_optimism', 'blockchain_polygon', 'token_pair_wstETH-wUSDM', 'token_pair_xSNXa-YFI', 'token_pair_yCURVE-YFI', 'day_of_week', 'month', 'year']
+    df_selected = df_encoded[selected_columns]
+
+    # Slide a window of this length across your time-series data
+    sequences_input = []
+    sequences_target = []
+
+    for i in range(total_sequences):
+        # Extract the historical data points as the input sequence
+        input_sequence = df_selected.iloc[i : i + window_size].values
+        sequences_input.append(input_sequence)
+        # Extract the next data point as the target for prediction
+        target = df_selected.iloc[i + window_size - 1, 2]  
+        sequences_target.append(target)
+
+    # Convert lists to numpy arrays
+    sequences_input = np.array(sequences_input)
+    sequences_target = np.array(sequences_target)
+
+    # Reshape the target sequences to match the shape of the input sequences
+    sequences_target = sequences_target.reshape(-1, 1)
+
+    sequences_input = sequences_input.astype(np.float32)
+    sequences_target = sequences_target.astype(np.float32)
+
+    return sequences_input, sequences_target
 
 
-# Define model hyperparameters
-selected_columns = ['trading_volume_usd', 'blockchain_arbitrum', 'blockchain_avalanche_c', 'blockchain_base', 'blockchain_ethereum', 'blockchain_gnosis', 'blockchain_optimism', 'blockchain_polygon', 'token_pair_wstETH-wUSDM', 'token_pair_xSNXa-YFI', 'token_pair_yCURVE-YFI', 'day_of_week', 'month', 'year']
-window_size = 30
-input_shape = (window_size, len(selected_columns))
-num_filters = 32
-num_blocks = 5
-kernel_size = 8
-num_classes = 1  # Assuming regression task, change to number of classes for classification task
-batch_size = 32
-
-loader = DatasetsLoader()
-# Load data from Polar into a DataFrame
-df_polar = loader.load('balancer-daily-trade-volume')
-
-# Extracting data from the Polar DataFrame
-data = {
-    'day': df_polar['day'],
-    'pool_id': df_polar['pool_id'],
-    'blockchain': df_polar['blockchain'],
-    'token_pair': df_polar['token_pair'],
-    'trading_volume_usd': df_polar['trading_volume_usd']
-}
-
-# Creating a new Pandas DataFrame
-df_pandas = pd.DataFrame(data)
-
-# Perform one-hot encoding for categorical variables
-df_encoded = pd.get_dummies(df_pandas, columns=['blockchain', 'token_pair'])
-
-# Initialize StandardScaler
-standard_scaler = StandardScaler()
-
-# Perform Standardization on numerical features
-df_encoded[['trading_volume_usd']] = standard_scaler.fit_transform(df_encoded[['trading_volume_usd']])
-
-# Convert 'day' column to datetime format
-df_encoded['day'] = pd.to_datetime(df_encoded['day'])
-
-# Extract relevant features: day of the week, month, and year
-df_encoded['day_of_week'] = df_encoded['day'].dt.dayofweek
-df_encoded['month'] = df_encoded['day'].dt.month
-df_encoded['year'] = df_encoded['day'].dt.year
-
-# Calculate the total number of data points
-total_data_points = df_encoded.shape[0]
-
-# Calculate the total number of sequences
-total_sequences = total_data_points - window_size + 1
-
-# Select only necessary columns from the DataFrame
-selected_columns = ['trading_volume_usd', 'blockchain_arbitrum', 'blockchain_avalanche_c', 'blockchain_base', 'blockchain_ethereum', 'blockchain_gnosis', 'blockchain_optimism', 'blockchain_polygon', 'token_pair_wstETH-wUSDM', 'token_pair_xSNXa-YFI', 'token_pair_yCURVE-YFI', 'day_of_week', 'month', 'year']
-df_selected = df_encoded[selected_columns]
-
-# Slide a window of this length across your time-series data
-sequences_input = []
-sequences_target = []
-
-for i in range(total_sequences):
-    # Extract the historical data points as the input sequence
-    input_sequence = df_selected.iloc[i : i + window_size].values
-    sequences_input.append(input_sequence)
-    
-    # Extract the next data point as the target for prediction
-    target = df_selected.iloc[i + window_size - 1, 2]  
-    sequences_target.append(target)
-
-# Convert lists to numpy arrays
-sequences_input = np.array(sequences_input)
-sequences_target = np.array(sequences_target)
-
-# Reshape the target sequences to match the shape of the input sequences
-sequences_target = sequences_target.reshape(-1, 1)
-
-sequences_input = sequences_input.astype(np.float32)
-sequences_target = sequences_target.astype(np.float32)
 
 @task(name="Prepare Datasets")
-def prepare_datasets():
+def prepare_datasets(df_encoded):
     print("Prepare dataset...")
 
     # Splitting into training and testing sets (80% train, 20% test)
@@ -125,6 +138,12 @@ class DataGenerator(Sequence):
         batch_targets = self.sequences_target[idx * self.batch_size : (idx + 1) * self.batch_size]
         return np.array(batch_inputs), np.array(batch_targets)
 
+@task(name="Test Model")
+def evaluate_model(model, X_test, y_test):
+    # Evaluate the model
+    test_loss, test_mae = model.evaluate(X_test, y_test)
+    print("Test Loss:", test_loss)
+    print("Test MAE:", test_mae)
 
 
 @task(name="Train model")
@@ -160,6 +179,25 @@ def build_wavenet(input_shape, num_filters, num_blocks, kernel_size, num_classes
     print("✅ Model trained successfully")
     return model
 
+@task(name="Visualize Data")
+def visualize_data(df_encoded):
+    # Convert 'day' column to datetime format if not already
+    df_encoded['day'] = pd.to_datetime(df_encoded['day'])
+
+    # Set the 'day' column as the index for time series plotting
+    df_encoded.set_index('day', inplace=True)
+
+    # Plot time series data
+    plt.figure(figsize=(12, 6))
+    for column in df_encoded.columns:
+        plt.plot(df_encoded.index, df_encoded[column], label=column)
+    plt.xlabel('Date')
+    plt.ylabel('Value')
+    plt.title('Time Series Plot')
+    plt.legend()
+    plt.show()
+
+    print("Time series plot generated.")
 
 
 
@@ -185,8 +223,14 @@ def convert_to_onnx(model, onnx_file_path):
 
 @action(name="Action: Convert To ONNX", log_prints=True)
 def execution():
+    input_shape, num_filters, num_blocks, kernel_size, num_classes, batch_size = define_model_hyperparameters()
+    df_polar = load_data()
+    df_encoded, window_size = preprocess_data(df_polar)
+    sequences_input, sequences_target = create_sequences(df_encoded, window_size)
+
+
     # Prepare datasets
-    X_train, y_train, X_test, y_test = prepare_datasets()
+    X_train, y_train, X_test, y_test = prepare_datasets(df_encoded)
 
     # Create data loaders
     data_generator = DataGenerator(sequences_input, sequences_target, batch_size)
@@ -205,17 +249,18 @@ def execution():
     data_generator = DataGenerator(sequences_input, sequences_target, batch_size)
 
     # Train the model using the fit method
-    model.fit(data_generator, epochs=1)
+    model.fit(data_generator, epochs=1) 
 
     # Evaluate the model
-    test_loss, test_mae = model.evaluate(data_generator)
-    print("Test Loss:", test_loss)
-    print("Test MAE:", test_mae)
+    evaluate_model(model, X_test, y_test)
 
 
     # Convert to ONNX
     onnx_file_path = "wavenet.onnx"
     convert_to_onnx(model, onnx_file_path)
+
+    # Visualize the data
+    visualize_data(df_encoded)
 
 
 # Create an Action object and serve it
